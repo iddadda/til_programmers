69b549c7ac3b1c217ba8728a5cb6c0353fa37108
iddadda <147607591+iddadda@users.noreply.github.com>
2025-12-02 22:43:59 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 14.02 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -2,20 +2,9 @@ class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
         
-        char[] str1 = my_string.toCharArray();
-        char[] str2 = overwrite_string.toCharArray();
-        
-        int len = str2.length+1;
-        int idx = 0;
-        
-        for(int i = 0; i < str2.length; i++){
-            str1[s] = str2[i];
-            s++;
-        }
-        
-       for(char digit : str1) {
-           answer += digit;
-       }
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        answer = before + overwrite_string + after;
         
         return answer;
     }
