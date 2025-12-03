5055acc0a08024c08541727a03e93b18581d7808
iddadda <147607591+iddadda@users.noreply.github.com>
2025-12-03 20:21:02 +0900

    [level 0] Title: 더 크게 합치기, Time: 10.10 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(int a, int b) {
-        
-        
-        String str1 = a + "";
-        String str2 = b + "";
-        
-        int solution1 = Integer.parseInt((str1 + str2));
-        int solution2 = Integer.parseInt((str2 + str1));
-        
-        int answer = solution1 < solution2 ? solution2 : solution1 ;
-        
-        return answer;
+        return Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a));
     }
 }
