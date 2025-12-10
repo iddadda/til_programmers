e66b25ad6d543906fdf232fbdfa443255388ba79
iddadda <147607591+iddadda@users.noreply.github.com>
2025-12-10 17:36:13 +0900

    [level 0] Title: 코드 처리하기, Time: 8.61 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,33 +1,19 @@
+
 class Solution {
     public String solution(String code) {
-        String answer = "";
-        String ret = "";
+        StringBuilder answer = new StringBuilder();
         int mode = 0;
-        int idx = 0;
-        
-//         문자열 -> 배열
-        char[] arr = code.toCharArray();
-        
-        
-//         "1"이면 mode 1로 변경하기
-        for(idx = 0; idx < arr.length; idx++) {
-            if(mode == 0) {
-                if(arr[idx] != '1') {
-                    ret += idx % 2 == 0 ? arr[idx] : "";
-                } else {
-                    mode = 1;    
-                }
-                
-            } else {
-                if(arr[idx] != '1') {
-                    ret += idx % 2 == 1 ? arr[idx] : "";
-                } else {
-                    mode = 0;    
-                }
-                
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            }
+
+            if (i % 2 == mode) {
+                answer.append(current);
             }
         }
-        
-        return "".equals(ret) ? "EMPTY" : ret ;
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
     }
 }
